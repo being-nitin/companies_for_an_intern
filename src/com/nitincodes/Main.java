0ff104da55db048150654cd42404c629f5d8de3f
Nitin <nsinghboss02@gmail.com>
2021-10-29 22:02:41 +0530

    companies in which we will be applying for an intern in third year

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,45 @@
+package com.nitincodes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	companies in which we will be applying in 3rd year for an internship:
+	1.Amazon
+	2.Microsoft
+	3.google
+	4.Adobe
+	5.Flipkart
+	6.goldman sachs
+	7.wallmart
+	8.Apple
+	9.Facebook
+	10.Zomato
+	11.Samsung
+	12. Netfilx
+	13.Tcs
+	14.LandRover
+	15.Jaguar
+	16.Delloite
+	17.Qualcomm
+	18.Wipro
+	19.Swiggy
+	20.Uber
+	21.Ola
+	22.DRDO
+	23.Credit suisse
+	24.Alibaba
+	25.Yahoo
+	26.Infosys
+	27.Reliance
+	28.Redhat
+	29.Rangerover
+	30.Mx player
+	31. Atlassiasn
+	32.IBM
+	33.Hackerrank
+	34.Linkedin
+	35.Unilever
+	 */
+    }
+}
